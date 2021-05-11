0cbfc0b3a1b948151629ff9d853a3ee41c67387f
030030 <74403092+030030@users.noreply.github.com>
2021-05-11 17:02:00 +0800

    Update Quiz20210511.aspx.cs

--- Quiz/Quiz20210511.aspx.cs
+++ Quiz/Quiz20210511.aspx.cs
@@ -9,7 +9,15 @@ using System.Web.UI.WebControls;
 namespace Quiz {
     public partial class Quiz20210511 : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
+           
+        }
 
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Session["ans1"] = TextBox1.Text;
+            Session["ans2"] = TextBox2.Text;
+            Response.Redirect("https://localhost:44321/Quiz20210511-1.aspx", false);
+            HttpContext.Current.ApplicationInstance.CompleteRequest();
         }
     }
 }
