7a08c7d119588be9509cf404825c01f8990493cb
030030 <74403092+030030@users.noreply.github.com>
2021-05-11 16:59:47 +0800

    Update Quiz20210511-1.aspx.cs

--- Quiz/Quiz20210511-1.aspx.cs
+++ Quiz/Quiz20210511-1.aspx.cs
@@ -9,7 +9,8 @@ using System.Web.UI.WebControls;
 namespace Quiz {
     public partial class Quiz202111-1 : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
-
+            Response.Write(Session["ans1"]+"<br>");
+            Response.Write(Session["ans2"] + "<br>");   
         }
     }
 }
